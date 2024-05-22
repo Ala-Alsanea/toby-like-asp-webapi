86e87d3eddc9e7fb5238aac86122958b60a1c62b
Ala-Alsanea <ala0alsanea@gmail.com>
2024-05-22 21:27:30 +0300

    ref: app builder

--- Config/AppBuilder.cs
+++ Config/AppBuilder.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using Topy_like_asp_webapi.Domain.Commands;
 using Topy_like_asp_webapi.Domain.DBContexts;
 using Topy_like_asp_webapi.Domain.Entities;
@@ -21,19 +25,31 @@ namespace Topy_like_asp_webapi.Config
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Logging.ClearProviders();
             builder.Logging.AddConsole();
-
+            
+            // R-Database registration
             // builder.Services.AddDbContext<DBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnectionString")));
             builder.Services.AddDbContext<DBContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnectionString")));
 
+            // Elasticsearch  registration
+            builder.Services.Configure<ElasticsearchSettings>(builder.Configuration.GetSection("Elasticsearch"));
+            builder.Services.AddSingleton<ElasticsearchClient>(sp =>
+            {
+                var settings = sp.GetRequiredService<IOptions<ElasticsearchSettings>>().Value;
+                var nodes = settings.Nodes.Select(n => new Uri(n)).ToArray();
+                var pool = new StaticNodePool(nodes);
+                var clientSettings = new ElasticsearchClientSettings(pool)
+                    // .CertificateFingerprint(settings.CertificateFingerprint)
+                    // .Authentication(new ApiKey(settings.ApiKey))
+                    ;
+                return new ElasticsearchClient(clientSettings);
+            });
 
-            // builder.Services.AddScoped<IRepository<User>, Repository<User>>();
-            // builder.Services.AddScoped<IRepository<Space>, Repository<Space>>();
-            // builder.Services.AddScoped<IRepository<Collection>, Repository<Collection>>();
-            // builder.Services.AddScoped<IRepository<Tab>, Repository<Tab>>();
-
-            builder.Services.AddScoped( typeof(IRepository<>), typeof(Repository<>));
-
+            // repository registration
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            builder.Services.AddScoped(typeof(IElasticsearchRepository<>), typeof(ElasticsearchRepository<>));
+            // CLI commands registration
             builder.Services.AddTransient<Seeder>();
+            builder.Services.AddTransient<ElasticsearchSeedr>();
             builder.Services.AddTransient<Truncate>();
 
             builder.Services.AddEndpointsApiExplorer();
